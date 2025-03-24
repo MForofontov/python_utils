95d7fe460a46a578675065d7e2dd7b2161acff1c
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-03-24 13:48:47 +0000

    refactor: make Graph class generic with type parameter

--- data_types/Graph.py
+++ data_types/Graph.py
@@ -1,21 +1,24 @@
-from typing import Dict, List, Any
+from typing import Dict, List, TypeVar, Generic
 
-class Graph:
+# Define a generic type variable
+T = TypeVar('T')
+
+class Graph(Generic[T]):
     """
     A Graph data structure using an adjacency list.
 
     Attributes
     ----------
-    adjacency_list : Dict[Any, List[Any]]
+    adjacency_list : Dict[T, List[T]]
         The dictionary representing the graph, where keys are vertices and values are lists of adjacent vertices.
 
     Methods
     -------
-    add_vertex(vertex)
+    add_vertex(vertex: T) -> None
         Adds a vertex to the graph.
-    add_edge(vertex1, vertex2)
+    add_edge(vertex1: T, vertex2: T) -> None
         Adds an edge between two vertices.
-    get_neighbors(vertex)
+    get_neighbors(vertex: T) -> List[T]
         Returns a list of neighbors for a given vertex.
     """
 
@@ -23,29 +26,29 @@ class Graph:
         """
         Initializes an empty graph.
         """
-        self.adjacency_list: Dict[Any, List[Any]] = {}
+        self.adjacency_list: Dict[T, List[T]] = {}
 
-    def add_vertex(self, vertex: Any) -> None:
+    def add_vertex(self, vertex: T) -> None:
         """
         Adds a vertex to the graph.
 
         Parameters
         ----------
-        vertex : Any
+        vertex : T
             The vertex to add.
         """
         if vertex not in self.adjacency_list:
             self.adjacency_list[vertex] = []
 
-    def add_edge(self, vertex1: Any, vertex2: Any) -> None:
+    def add_edge(self, vertex1: T, vertex2: T) -> None:
         """
         Adds an edge between two vertices.
 
         Parameters
         ----------
-        vertex1 : Any
+        vertex1 : T
             The first vertex.
-        vertex2 : Any
+        vertex2 : T
             The second vertex.
         """
         if vertex1 not in self.adjacency_list:
@@ -55,18 +58,18 @@ class Graph:
         self.adjacency_list[vertex1].append(vertex2)
         self.adjacency_list[vertex2].append(vertex1)
 
-    def get_neighbors(self, vertex: Any) -> List[Any]:
+    def get_neighbors(self, vertex: T) -> List[T]:
         """
         Returns a list of neighbors for a given vertex.
 
         Parameters
         ----------
-        vertex : Any
+        vertex : T
             The vertex whose neighbors are to be returned.
 
         Returns
         -------
-        List[Any]
+        List[T]
             A list of neighbors for the given vertex.
         """
         return self.adjacency_list.get(vertex, [])
