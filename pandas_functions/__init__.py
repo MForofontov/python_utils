cb5e69739d0df1349e4d808aa5eba18eb5689587
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-08-20 17:24:35 +0100

    Add merge_dataframes function and tests

--- pandas_functions/__init__.py
+++ pandas_functions/__init__.py
@@ -1,7 +1,9 @@
 from .dict_to_df import dict_to_df
 from .import_df_from_file import import_df_from_file
+from .merge_dataframes import merge_dataframes
 
 __all__ = [
     "dict_to_df",
     "import_df_from_file",
+    "merge_dataframes",
 ]
