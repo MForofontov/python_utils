8f658bf70f7510c7c2ad31e99d74266d48394a17
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-08-20 17:36:57 +0100

    Add DataFrame concatenation utility

--- pandas_functions/__init__.py
+++ pandas_functions/__init__.py
@@ -1,7 +1,9 @@
+from .concat_dfs import concat_dfs
 from .dict_to_df import dict_to_df
 from .import_df_from_file import import_df_from_file
 
 __all__ = [
+    "concat_dfs",
     "dict_to_df",
     "import_df_from_file",
 ]
