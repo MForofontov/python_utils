de058d1f00792573414bd3557756ad5f02faf2ed
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-08-20 17:08:19 +0100

    Add dataframe to dictionary conversion utility

--- pandas_functions/__init__.py
+++ pandas_functions/__init__.py
@@ -1,7 +1,9 @@
 from .dict_to_df import dict_to_df
 from .import_df_from_file import import_df_from_file
+from .df_to_dict import df_to_dict
 
 __all__ = [
     "dict_to_df",
     "import_df_from_file",
+    "df_to_dict",
 ]
