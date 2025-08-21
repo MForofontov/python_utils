dc7ec2ced94ddc49456bf0e1e13e2077892f237b
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-08-21 18:32:00 +0100

    Add tests for elementwise product??

--- numpy_functions/__init__.py
+++ numpy_functions/__init__.py
@@ -2,6 +2,7 @@ from .array_mean import array_mean
 from .array_variance import array_variance
 from .dot_product import dot_product
 from .elementwise_sum import elementwise_sum
+from .elementwise_product import elementwise_product
 from .matrix_multiply import matrix_multiply
 from .normalize_array import normalize_array
 
@@ -10,6 +11,7 @@ __all__ = [
     "array_variance",
     "dot_product",
     "elementwise_sum",
+    "elementwise_product",
     "matrix_multiply",
     "normalize_array",
 ]
