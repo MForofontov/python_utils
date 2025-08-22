ae43bca372ba0bdc016c72ba9dfaeac410ab6663
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-08-22 16:43:02 +0100

    Add array product utility with tests

--- numpy_functions/__init__.py
+++ numpy_functions/__init__.py
@@ -5,6 +5,7 @@ from .array_median import array_median
 from .array_max import array_max
 from .array_min import array_min
 from .array_sum import array_sum
+from .array_prod import array_prod
 from .dot_product import dot_product
 from .elementwise_sum import elementwise_sum
 from .elementwise_product import elementwise_product
@@ -22,6 +23,7 @@ __all__ = [
     "array_max",
     "array_min",
     "array_sum",
+    "array_prod",
     "dot_product",
     "elementwise_sum",
     "elementwise_product",
