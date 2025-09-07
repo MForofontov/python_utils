93acf8a1fd13112a18e66bb407e6cdd3954e9118
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2025-09-07 16:02:18 +0100

    Refactor module imports: change import structure to improve error handling and maintainability

--- __init__.py
+++ __init__.py
@@ -6,24 +6,45 @@ organized into specialized modules for enterprise-grade utility libraries.
 """
 
 # Import all modules
-from .asyncio_functions import *
-from .compression_functions import *
-from .data_types import *
-from .data_validation import *
-from .datetime_functions import *
-from .decorators import *
-from .env_config_functions import *
-from .file_functions import *
-from .http_functions import *
-from .iterable_functions import *
-from .json_functions import *
-from .linux_functions import *
-from .logger_functions import *
-from .mathematical_functions import *
-from .multiprocessing_functions import *
-from .print_functions import *
-from .security_functions import *
-from .strings_utility import *
+try:
+    from asyncio_functions import *
+    from compression_functions import *
+    from data_types import *
+    from data_validation import *
+    from datetime_functions import *
+    from decorators import *
+    from env_config_functions import *
+    from file_functions import *
+    from http_functions import *
+    from iterable_functions import *
+    from json_functions import *
+    from linux_functions import *
+    from logger_functions import *
+    from mathematical_functions import *
+    from multiprocessing_functions import *
+    from print_functions import *
+    from security_functions import *
+    from strings_utility import *
+except ImportError:
+    # Fallback for when importing as a package
+    from .asyncio_functions import *
+    from .compression_functions import *
+    from .data_types import *
+    from .data_validation import *
+    from .datetime_functions import *
+    from .decorators import *
+    from .env_config_functions import *
+    from .file_functions import *
+    from .http_functions import *
+    from .iterable_functions import *
+    from .json_functions import *
+    from .linux_functions import *
+    from .logger_functions import *
+    from .mathematical_functions import *
+    from .multiprocessing_functions import *
+    from .print_functions import *
+    from .security_functions import *
+    from .strings_utility import *
 
 # Version information
 __version__ = '1.0.0'
