c2e2879c6cfafaf7370e3c688aecf69cff5993ae
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2025-09-07 17:29:51 +0100

    Remove obsolete test files for statistical functions
    - Deleted test files for range_value, skewness, random floats, random integers, random normal, random sample, standard deviation, time series analysis, and variance.
    - These deletions are part of a cleanup process to remove outdated or redundant tests that are no longer necessary for the current implementation.

--- mathematical_functions/__init__.py
+++ mathematical_functions/__init__.py
@@ -18,35 +18,11 @@ from .statistics import *
 from .random import *
 
 # Re-export main functions for convenience
-from .statistics.mean import mean
-from .statistics.median import median, weighted_median
-from .statistics.mode import mode
-from .statistics.stddev import stddev
-from .statistics.variance import variance
-from .statistics.range_value import range_value
-from .statistics.quantile import quantile
-from .statistics.skewness import skewness
-from .statistics.correlation_analysis import correlation_analysis
-from .statistics.correlation_coefficient import correlation_coefficient
-from .statistics.time_series_analysis import time_series_analysis
-from .statistics.linear_regression import linear_regression
-from .statistics.chi_square_test import chi_square_test
-from .statistics.outlier_detection import outlier_detection
+# Statistics functions moved to external packages (scipy, numpy, statistics)
 
 __all__ = [
     # Basic math operations
     "power",
-    
-    # Statistics
-    "mean", "median", "weighted_median", "mode", "stddev", "variance", 
-    "range_value", "quantile", "skewness",
-    
-    # Correlation and analysis
-    "correlation_analysis", "correlation_coefficient",
-    "time_series_analysis", "linear_regression",
-    
-    # Statistical tests
-    "chi_square_test", "outlier_detection",
     
     # Random generators
     "random_integers", "random_floats", "random_normal", "random_sample",
