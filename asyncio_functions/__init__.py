56414c09131de8854cc3664d7e7918da74271f84
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2025-09-09 16:17:19 +0100

    Refactor __init__.py to include async_download and async_parallel_download functions, and update __all__ exports accordingly

--- asyncio_functions/__init__.py
+++ asyncio_functions/__init__.py
@@ -5,17 +5,18 @@ from .async_chain import async_chain
 from .async_cleanup import async_cleanup
 from .async_connection_pool import AsyncConnectionPool, use_connection
 from .async_event_loop import async_event_loop
-from .async_map import async_map
 from .async_periodic import async_periodic
 from .async_rate_limited import async_rate_limited
 from .async_retry_with_backoff import async_retry_with_backoff
 from .async_stream_processor import async_stream_processor
 from .async_throttle import async_throttle
-from .async_timeout import async_timeout
-from .fetch_all import fetch, fetch_all
+from .async_download import async_download
+from .async_parallel_download import async_parallel_download
+from .fetch_url import fetch_url
+from .fetch_multiple_urls import fetch_multiple_urls
 from .gather_with_timeout import gather_with_timeout
 from .retry_async import retry_async
-from .run_in_parallel import run_in_parallel
+
 
 __all__ = [
     "async_await_with_error_handling",
@@ -26,16 +27,15 @@ __all__ = [
     "AsyncConnectionPool",
     "use_connection",
     "async_event_loop",
-    "async_map",
     "async_periodic",
     "async_rate_limited",
     "async_retry_with_backoff",
     "async_stream_processor",
     "async_throttle",
-    "async_timeout",
-    "fetch",
-    "fetch_all",
+    "async_download",
+    "async_parallel_download",
+    "fetch_url",
+    "fetch_multiple_urls",
     "gather_with_timeout",
     "retry_async",
-    "run_in_parallel",
 ]
