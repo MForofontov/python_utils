31bd033f71fcb41b6da339b2fc67d1363075703d
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2025-10-09 17:39:01 +0100

    Remove implementations of various data structures including Linked List, Red-Black Tree, Segment Tree, Skip List, Splay Tree, and Trie. Also, delete associated unit tests for these data structures.

--- data_types/__init__.py
+++ data_types/__init__.py
@@ -1,34 +1,9 @@
-from .avl_node import AVLNode, AVLTree
-from .binary_tree import BinaryTree, BinaryTreeNode
 from .circular_queue import CircularQueue
 from .graph import Graph
-from .hash_table import HashTable
-from .linked_list import LinkedList, Node
-from .red_black_tree import RedBlackNode, RedBlackTree
-from .segment_tree import SegmentTree
-from .skip_list import SkipList, SkipNode
-from .splay_tree import SplayNode, SplayTree
-from .trie import Trie, TrieNode
 from .union_find import UnionFind
 
 __all__ = [
-    "AVLNode",
-    "AVLTree",
-    "BinaryTreeNode",
-    "BinaryTree",
     "CircularQueue",
     "Graph",
-    "HashTable",
-    "Node",
-    "LinkedList",
-    "RedBlackNode",
-    "RedBlackTree",
-    "SegmentTree",
-    "SkipNode",
-    "SkipList",
-    "SplayNode",
-    "SplayTree",
-    "TrieNode",
-    "Trie",
     "UnionFind",
 ]
