582366bfe8d02cab0b9df0d0343868667e67aca4
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2025-12-19 12:29:49 +0000

    feat: Enhance dev utilities with additional functions and update exports

--- dev_utilities/__init__.py
+++ dev_utilities/__init__.py
@@ -13,12 +13,15 @@ from . import (
 )
 
 # Import all functions for direct access
-from .code_analysis.count_code_lines import count_code_lines
+from .code_analysis.count_code_lines import count_code_lines, count_code_lines_directory, CodeLineCount
 from .code_analysis.generate_dependency_graph import generate_dependency_graph
-from .code_analysis.get_code_statistics import get_code_statistics
-from .license_templates.generate_license import generate_license
+from .code_analysis.get_code_statistics import get_code_statistics, format_statistics, CodeStatistics
+from .license_templates.generate_license import generate_license, save_license_file, LicenseType
 from .project_validation.validate_project_structure import (
     validate_project_structure,
+    format_validation_result,
+    ValidationResult,
+    ValidationIssue,
 )
 
 __all__ = [
@@ -28,12 +31,22 @@ __all__ = [
     "project_validation",
     # Code analysis functions
     "count_code_lines",
+    "count_code_lines_directory",
     "generate_dependency_graph",
     "get_code_statistics",
+    "format_statistics",
     # License functions
     "generate_license",
+    "save_license_file",
     # Project validation functions
     "validate_project_structure",
+    "format_validation_result",
+    # Data classes
+    "CodeLineCount",
+    "CodeStatistics",
+    "ValidationResult",
+    "ValidationIssue",
+    "LicenseType",
 ]
 
 __version__ = "1.0.0"
