621b46bcbfeb37859d861fe055a35cd461329704
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2026-01-21 12:09:27 +0000

    feat: Add central version definition for the package

---
+++ _version.py
@@ -0,0 +1,8 @@
+"""
+Central version definition for the entire package.
+
+This module provides a single source of truth for version information
+that can be imported and reused across all submodules.
+"""
+
+__version__ = "0.1.0"
